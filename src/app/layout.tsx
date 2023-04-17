6526f66050371e015c2d9c02e90601304d467485
Luis Irias <luis.irias@aol.com>
2023-04-17 14:47:14 -0600

    Revert "Feat"
    This reverts commit 6abdb44eab6bf61279723ac186bca05ce41524e1.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,30 @@
-import { Nunito } from 'next/font/google';
+import { Nunito } from 'next/font/google'
 
-import './globals.css';
+import './globals.css'
 
 // Components
-import ClientOnly from './components/ClientOnly';
-import NavBar from './components/navbar/NavBar';
-import RegisterModal from './components/modals/RegisterModal';
-import ToasterProvider from './providers/ToasterProvider';
+import NavBar from './components/navbar/NavBar'
 
 export const metadata = {
   title: 'Airbnb',
   description: 'Airbnb clone',
-};
+}
 
-const font = Nunito({ subsets: ['latin'] });
+const font = Nunito({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={font.className}>
-        <ClientOnly>
-          <ToasterProvider />
-          <RegisterModal />
-          <NavBar />
-        </ClientOnly>
+        <NavBar />
 
         {children}
-      </body>
+        
+        </body>
     </html>
-  );
+  )
 }
--- src/app/providers/ToasterProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { Toaster } from 'react-hot-toast';
-
-// We need this wrapper to use a client-only npm packages because they need at least one client component parent ('use client')
-const ToasterProvider = () => <Toaster />;
-
-export default ToasterProvider;
