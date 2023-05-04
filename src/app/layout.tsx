83e457bef7c85d708a10b68edb84af694ce476c5
Luis Irias <luis.irias@aol.com>
2023-05-04 15:54:39 -0600

    Feat: Search filtering
    getListings action method now applies more filters depending of the query string
    - search component at navbar now is functional
    - SearchModal created to apply that filters
    - useSearchModal hook created to control the SearchModal component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import NavBar from './components/navbar/NavBar';
 import RegisterModal from './components/modals/RegisterModal';
 import LoginModal from './components/modals/LoginModal';
 import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
 
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
@@ -36,6 +37,7 @@ export default async function RootLayout({
           <RegisterModal />
           <LoginModal />
           <RentModal />
+          <SearchModal />
 
           <NavBar currentUser={currentUser} />
         </ClientOnly>
